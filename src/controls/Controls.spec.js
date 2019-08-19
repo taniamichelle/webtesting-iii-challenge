10711e50b93ac0ee9e2f344f5ca908f5e7d6cd38
tania <tanislavens@yahoo.com>
2019-08-19 15:26:44 -0400

    wip: passed matches snapshot tests on all three components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import Controls from './Controls';
+import {render, fireEvent} from '@testing-library/react'; 
+
+describe('<Controls />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Controls />); // generates a DOM tree
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+  // What we want to test: 
+    // when user clicks button, what happens in the app?
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import Display from './Display';
+import {render, fireEvent} from '@testing-library/react'; 
+
+describe('<Display />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Display />); // generates a DOM tree
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+  // What we want to test: 
+    // when user clicks button, what happens in the app?
+});
